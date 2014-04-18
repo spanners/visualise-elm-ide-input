48903ff64a8958fc6e123fbc9fc1b392d24050db
Simon Buist <simon.buist@gmail.com>
2014-04-18 01:06:41 +0100

    Visualise mouse data

--- StampTogether/helper.js
+++ StampTogether/helper.js
@@ -1,4 +1,4 @@
-var firebaseData = new Firebase('https://thsoft.firebaseio-demo.com/stamps');
+var firebaseData = new Firebase('https://sweltering-fire-9141.firebaseio.com/dissertation/js/12');
 var elm = Elm.fullscreen(Elm.StampTogether.Main);
 firebaseData.on('child_added', function(snapshot) {
 	elm.send('stamped', snapshot.val());
